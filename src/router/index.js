6361acc4aece19c1ce04f774fee8dcc3da44b01d
FaridIzquierdo <89101074+FaridIzquierdo@users.noreply.github.com>
2024-05-21 13:12:17 -0500

    Add validation to enter home view

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,10 +14,28 @@ const router = createRouter({
       path: '/forgot-password', name: 'forgot-password', component: () => import('../views/forgotPassword.vue')
     },
     {
-      path: '/home', name: 'home', component: () => import('../views/home.vue')
+      path: '/home', name: 'home', component: () => import('../views/home.vue'),  meta: { requiresAuth: true }
     }
   ]
 })
+
+
+router.beforeEach((to, from, next) => {
+  const auth = getAuth();
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth) {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        next();
+      } else {
+        next({ name: 'login' });
+      }
+    });
+  } else {
+    next();
+  }
+});
 
 router.afterEach(() => {
   window.scrollTo(0, 0);
