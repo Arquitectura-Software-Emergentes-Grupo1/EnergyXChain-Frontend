5adbfb2bd3ec3d3b769d7fa870d3a36e830e2db8
KendallRcs <kendallramiro@gmail.com>
2024-06-24 01:35:20 -0500

    added nav-bar for supplier

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,13 @@ import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 import NavbarLogin from './components/navbarLogin.vue'
 import NavbarClient from './components/navbarClient.vue'
+import NavbarSupplier from './components/navbarSupplier.vue';
 import { useGlobalStore } from '@/stores/globalStore';
 
 const route = useRoute();
 const globalStore = useGlobalStore();
 const sharedVariable = computed(() => globalStore.sharedVariable);
 
-
-const showNavbarLogin = computed(() => {
-  return route.path === '/' || route.path === '/main-register';
-});
-const showNavbarClient = computed(() => {
-  return route.path === '/proveedores' || route.path === '/historial' || route.path === '/pagar' || route.path === '/proveedorInfo' || route.path === '/home' || route.path === '/pagos' || route.path === '/pagar' || route.path === '/proveedor/id';
-});
-const showNavbarSupplier = computed(() => {
-  return route.path === '/homeProveedor' || route.path === '/servicios' || route.path === '/nuevo-servicio' || route.path === '/my-clients' || route.path === '/client-info/:id' || route.path === '/ganancias';
-});
 </script>
 
 <template>
@@ -27,7 +18,7 @@ const showNavbarSupplier = computed(() => {
   <header>
     <navbarLogin v-if="sharedVariable == 'login'"></navbarLogin >
     <navbarClient v-if="sharedVariable == 'client'"></navbarClient >
-      <p v-if="sharedVariable == 'supplier'">HASHHASHHAS</p>
+      <NavbarSupplier v-if="sharedVariable == 'supplier'"></NavbarSupplier>
   </header>
 
   <body>
