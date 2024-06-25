a6c008e4d6b31e1b866a10e8e1da7a9233586d0a
Farid Josué Izquierdo Cristobal <u202015215@upc.edu.pe>
2024-06-25 04:27:53 -0500

    update

--- src/views/proveedorInfo.vue
+++ src/views/proveedorInfo.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container">
-    <h1>{{ proveedorData.name }}</h1>
-    <div class="proveedorContainer">
-      <div class="proveedorInfo">
-        <p><span style="font-weight: bold;">Empresa:</span> {{ proveedorData.name }}</p>
-        <p><span style="font-weight: bold;">Teléfono:</span> {{ proveedorData.phone }}</p>
-        <p><span style="font-weight: bold;">Precio:</span> {{ proveedorData.price }}</p>
-        <p><span style="font-weight: bold;">Correo:</span> {{ proveedorData.email }}</p>
+    <h1>{{ planData.name }}</h1>
+    <div class="planContainer">
+      <div class="planInfo">
+        <p><span style="font-weight: bold;">Nombre:</span> {{ planData.name }}</p>
+        <p><span style="font-weight: bold;">Descripción:</span> {{ planData.description }}</p>
+        <p><span style="font-weight: bold;">Tarifa:</span> {{ planData.fee }}</p>
+        <p><span style="font-weight: bold;">Proveedor:</span> {{ planData.supplier.name }}</p>
       </div>
-      <div class="proveedorCTA">
-        <img src="../assets/img/proveedor.webp" alt="Proveedor" style="border-radius: 50%; width: 250px;">
+      <div class="planCTA">
+        <!-- Aquí puedes agregar cualquier botón o acción relacionada con el plan -->
         <Button @click="hire()" label="Contratar" style="background-color: #024955; border: #024955; width: 70%;" raised />
       </div>
     </div>
-    <div class="proveedorDesc">
-      <p><span style="font-weight: bold;">Descripción:</span></p>
-      <p>{{ proveedorData.description }}</p>
+    <div class="planDesc">
+      <p><span style="font-weight: bold;">Detalles del plan:</span></p>
+      <p>{{ planData.description }}</p>
     </div>
-    <button class="logout-button" @click="logout()">Log Out</button>
+    <button class="logout-button" @click="logout()">Cerrar sesión</button>
   </div>
 </template>
 
@@ -28,14 +28,15 @@ import { getAuth, signOut } from 'firebase/auth';
 import { API_BASE_URL } from '@/config';
 
 const router = useRouter();
-const proveedorData = ref({
+const planData = ref({
   id: null,
   name: '',
-  companyName: '',
-  cellphone: '',
-  price: null,
   description: '',
-  email: ''
+  fee: null,
+  supplier: {
+    id: null,
+    name: ''
+  }
 });
 
 onMounted(async () => {
@@ -47,7 +48,7 @@ onMounted(async () => {
 
   const { id } = router.currentRoute.value.params;
   try {
-    const response = await fetch(`${API_BASE_URL}api/v0/supplier/${id}`, {
+    const response = await fetch(`${API_BASE_URL}api/v0/plan/${id}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -55,12 +56,12 @@ onMounted(async () => {
       }
     });
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error('Error al obtener los datos del plan');
     }
     const jsonData = await response.json();
-    proveedorData.value = jsonData;
+    planData.value = jsonData;
   } catch (error) {
-    console.error('Error fetching supplier data:', error);
+    console.error('Error fetching plan data:', error);
   }
 });
 
@@ -72,7 +73,7 @@ const logout = () => {
       router.push('/');
     })
     .catch((error) => {
-      console.error('Error logging out:', error);
+      console.error('Error al cerrar sesión:', error);
     });
 };
 
@@ -103,25 +104,25 @@ h1 {
   align-items: center;
   gap: 20px;
 }
-.proveedorContainer {
+.planContainer {
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 20px;
   margin-top: 20px;
 }
-.proveedorInfo {
+.planInfo {
   background-color: #f0f0f0;
   padding: 20px;
   border-radius: 8px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
-.proveedorCTA {
+.planCTA {
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 10px;
 }
-.proveedorDesc {
+.planDesc {
   background-color: #f0f0f0;
   padding: 20px;
   border-radius: 8px;
